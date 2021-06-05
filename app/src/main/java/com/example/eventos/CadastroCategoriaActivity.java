02c465f71593c592eaa73d380548b25b39913daa
cmcleon <77648947+cmcleon@users.noreply.github.com>
2021-06-04 21:28:04 -0300

    :up: aula final

--- app/src/main/java/com/example/eventos/CadastroCategoriaActivity.java
+++ app/src/main/java/com/example/eventos/CadastroCategoriaActivity.java
@@ -6,7 +6,9 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.example.eventos.database.Entity.CategoriaDAO;
@@ -17,6 +19,7 @@ public class CadastroCategoriaActivity extends AppCompatActivity {
 
     private int id = 0;
     private EditText editTextDescricao;
+
 
     @SuppressLint("WrongViewCast")
     @Override
--- app/src/main/java/com/example/eventos/CadastroEventoActivity.java
+++ app/src/main/java/com/example/eventos/CadastroEventoActivity.java
@@ -5,11 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.text.TextWatcher;
+import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.example.eventos.database.Entity.CategoriaDAO;
 import com.example.eventos.database.EventoDAO;
+import com.example.eventos.modelo.Categoria;
 import com.example.eventos.modelo.Evento;
 
 import java.text.DateFormat;
@@ -22,6 +26,12 @@ public class CadastroEventoActivity extends AppCompatActivity {
 //
 //    private boolean edicao = false;
     private int id = 0;
+    private Spinner spinnerCategorias;
+    private ArrayAdapter<Categoria> categoriasAdapter;
+    private EditText editTextNome;
+    private EditText editTextLocal;
+    private EditText editTextDate;
+
 
 
     @Override
@@ -33,8 +43,22 @@ public class CadastroEventoActivity extends AppCompatActivity {
         EditText editTextData = findViewById(R.id.editTextDate_data);
         TextWatcher textWatcher = MaskEditUtil.mask(editTextData, MaskEditUtil.FORMAT_DATE);
         editTextData.addTextChangedListener(textWatcher);
+
+        spinnerCategorias = findViewById(R.id.spinner_categorias);
+        editTextNome = findViewById(R.id.editText_nome);
+        editTextLocal = findViewById(R.id.editText_local);
+        editTextDate = findViewById(R.id.editTextDate_data);
+        carregarCategorias();
         carregarEvento();
 
+    }
+
+    private void carregarCategorias(){
+        CategoriaDAO categoriaDAO = new CategoriaDAO(getBaseContext());
+        categoriasAdapter = new ArrayAdapter<Categoria>(CadastroEventoActivity.this,
+                android.R.layout.simple_spinner_item,
+                categoriaDAO.listar());
+        spinnerCategorias.setAdapter(categoriasAdapter);
     }
 
     private void carregarEvento() {
@@ -44,12 +68,23 @@ public class CadastroEventoActivity extends AppCompatActivity {
             EditText editTextNome = findViewById(R.id.editText_nome);
             EditText editTextLocal = findViewById(R.id.editText_local);
             EditText editTextData = findViewById(R.id.editTextDate_data);
+            int posicaoCategoria = obterPosicaoCategoria(evento.getCategoria());
+            spinnerCategorias.setSelection(posicaoCategoria);
             editTextNome.setText(evento.getNome());
             editTextLocal.setText(String.valueOf(evento.getLocal()));
             editTextData.setText(String.valueOf(evento.getData()));
- //         edicao = true;
+
             id = evento.getId();
         }
+    }
+
+    private int obterPosicaoCategoria(Categoria categoria){
+        for (int posicao = 0; posicao < categoriasAdapter.getCount(); posicao++){
+            if (categoriasAdapter.getItem(posicao).getId() == categoria.getId()){
+                return posicao;
+            }
+        }
+        return  0;
     }
 
     public void onClickVoltar(View v) {
@@ -61,9 +96,13 @@ public class CadastroEventoActivity extends AppCompatActivity {
         EditText editTextLocal = findViewById(R.id.editText_local);
         EditText editTextData = findViewById(R.id.editTextDate_data);
 
+
         String nome = editTextNome.getText().toString();
         String local = editTextLocal.getText().toString();
         String dataString = editTextData.getText().toString();
+        //Categoria categoria =(Categoria) spinnerCategorias.getSelectedItem();
+        int posicaoCategoria = spinnerCategorias.getSelectedItemPosition();
+        Categoria categoria =(Categoria) categoriasAdapter.getItem(posicaoCategoria);
         /*DateFormat formatar = new SimpleDateFormat("dd-MM-yyyy");
         Date data = formatar.parse(dataString);*/
 
@@ -74,12 +113,7 @@ public class CadastroEventoActivity extends AppCompatActivity {
         }else if(!validarCampo(dataString)) {
         mostrarErro(editTextData);
         }else{
-            Evento evento = new Evento(id, nome, local, dataString, null);
-//            Intent intent = new Intent();
-//            if (edicao) {
-//                intent.putExtra("eventoEditado", evento);
-//                setResult(RESULT_CODE_EVENTO_EDITADO, intent);
-//            } else {
+            Evento evento = new Evento(id, nome, local, dataString, categoria);
             EventoDAO eventoDAO = new EventoDAO(getBaseContext());
             boolean salvou = eventoDAO.salvar(evento);
             if(salvou){
